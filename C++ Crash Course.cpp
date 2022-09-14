ac00e6552980058c23adaf0b977c8222148844c8
NicDuncanUNF <pattonsboy@gmail.com>
2022-09-14 13:50:08 -0400

    Added preprocessor directives such as the max tokens that can be expected and conditional compilation of debug code. Added a method to print all tokens that were retieved. Begun the process of creating the tokenizer for the input string. Currently it errors out with a memory access violation.

--- C++ Crash Course.cpp
+++ C++ Crash Course.cpp
@@ -3,6 +3,10 @@
 #include <string>
 #include <map>
 using namespace std;
+//Preprocessor directive for enabling debug mode, leverages conditional compilation to help leave out unneeded debug code.
+//Use cerr for the standard C++ error IO stream
+#define DEBUGMODE true
+#define MAXSIGTOKENS 10
 
 //Program Goal: To take in a SIG code statement, and output it in plain english.
 /*Example:
@@ -32,28 +36,42 @@ using namespace std;
 //initialize methods
 map<string, string> mapBuilder();
 void printMap(map<string, string>);
+void printTokens(string tokens[]);
 
 int main()
 {
+    #ifdef DEBUGMODE
+        cerr << "\nDEBUG: Starting in main\n";
+    #endif
     //initialize important variables
-    string sigInput;
-    //An array of string to house the SIG statement tokens, statements that I have seen are usually no longer than 5ish tokens
-    string brokenUpInput[10];
+    char sigInput[] = "";
+    //An array of string to house the SIG statement tokens, statements that I have seen are usually no longer than 5ish tokens. This will be adjusted after some research.
+    string brokenUpInput[MAXSIGTOKENS];
     //A map data structure that will use the SIG codes as keys and the plain english equivalent as the mapped value
     map<string, string> SIGList = mapBuilder();
+    //Context parameter for strtok_s, a pointer to help keep track of what string is read and where on the string is read next.
+    char *inputContext = NULL;
 
 
-
-
+    //Main loop of the program
     while (true) {
         //Prompt the user for a SIG statement
-        cout << "Please enter your SIG statement. Please ensure there is a space between each part.\n";
+        cout << "Please enter your SIG statement. Please ensure there is a space between each part and that all caps are used.\n";
         cin >> sigInput;
         cout << "Received statement: " << sigInput;
 
         //Loop to break down the statement into an array of individual tokens.
+        brokenUpInput[0] = strtok_s(sigInput, " ", &inputContext);
+        int iteration = 1;
+        while (sigInput != NULL) {
+            brokenUpInput[iteration] = strtok_s(NULL, " ", &inputContext);
+            iteration++;
+        }//end while
 
-
+        //If in debug mode, print out all tokens recieved
+        #ifdef DEBUGMODE
+            printTokens(brokenUpInput);
+        #endif
 
     }//end while
 
@@ -64,6 +82,9 @@ int main()
 
 //A function meant to construct the map and return it to main
 map<string, string> mapBuilder() {
+    #ifdef DEBUGMODE
+        cerr << "\nDEBUG: Starting in mapBuilder\n";
+    #endif
     map<string, string> temp;
     //A testing example
     temp["BID"] = "twice a day";
@@ -74,6 +95,20 @@ map<string, string> mapBuilder() {
 
 //A function to print out the contents of a map in a neat way for testing purposes
 void printMap(map<string, string>) {
+    #ifdef DEBUGMODE
+        cerr << "\nDEBUG: Starting in printMap\n";
+    #endif
     //TODO
     return;
 }
+
+//A function to print all found tokens for debug purposes.
+void printTokens(string tokens[]) {
+    #ifdef DEBUGMODE
+        cerr << "\nDEBUG: Starting in printTokens\n";
+    #endif
+    for (int i = 0; i < sizeof(tokens); i++) {
+        cout << "TOKEN: " << tokens[i] << "\n";
+
+    }//end if
+}//end function
